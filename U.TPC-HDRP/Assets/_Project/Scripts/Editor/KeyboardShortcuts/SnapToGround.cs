5017c26b03ebdb4c3f2d99769795f522ff67bebd
noelmurillo15 <noelmurillo15@fullsail.edu>
2022-09-15 11:26:33 -0700

    v0.1.0
    - removed Yggdrasil environment (11.6gb)
    - enabled raytracing graphics
    - fixed character model

---
+++ U.TPC-HDRP/Assets/_Project/Scripts/Editor/KeyboardShortcuts/SnapToGround.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ANM.Editor
+{
+    public class SnapToGround : MonoBehaviour
+    {
+        [MenuItem("Custom/Snap To Ground %g")]
+        public static void Ground()
+        {
+            foreach (var transform in Selection.transforms)
+            {
+                var hits = Physics.RaycastAll(transform.position + Vector3.up, Vector3.down, 15f);
+                foreach (var raycastHit in hits)
+                {
+                    if(raycastHit.collider.gameObject == transform.gameObject)
+                        continue;
+
+                    transform.position = raycastHit.point;
+                    break;
+                }
+            }
+        }
+    }
+}
